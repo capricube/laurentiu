eecb1e84003711e7798da5319dfa1d7d3e72c5ff
Kristian.Marinkovic <Kristian.Marinkovic@gmail.com>
2013-11-10 16:51:50 +0100

    TODO and FIX comments added

--- sortingalgorithm/src/Sortieralgorithmen/BubbleSort.java
+++ sortingalgorithm/src/Sortieralgorithmen/BubbleSort.java
@@ -1,19 +1,23 @@
 package Sortieralgorithmen;
-
+// TODO package namen müssen immer englisch sein; ebenso alle begriffe im code
+// TODO formattiere den code mit dem code formatter in eclipse
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		
+	    // TODO umbenennen
 		int Werte[] = {6,2,8,1,4,5,7,9,3};
 		
 		int temp;
 		
+		// FIXME code duplication: entferne doppelten code; doppelter code bedeutet mehr arbeit und schlechtere wartbarkeit
 		System.out.print("Unsortierte Werte: \n");
 		for(int i=0; i<Werte.length; i++){
 			System.out.print(Werte[i]+" ");
 		}
 		System.out.println();
 		
+		// TODO instanziere die Bubblesort Klasse und starte die Algorithmus indem du der sort methode das array übergibst. das Ergebnis der sort Methode ist dar sortierte array. 
 		boolean fertigGetauscht=false;
 		while(fertigGetauscht==false){
 			fertigGetauscht=true;
@@ -28,6 +32,8 @@ public class BubbleSort {
 				
 			}
 		}
+		
+        // FIXME code duplication: entferne doppelten code
 		System.out.print("Sortierte Werte: \n");
 		for(int i=0; i<Werte.length; i++){
 			System.out.print(Werte[i]+" ");
