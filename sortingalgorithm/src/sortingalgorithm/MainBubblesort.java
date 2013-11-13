1e672a70f58530182ed917551c01c0e1b854d77a
Laurentiu84 <vladlaurentiu@hotmail.com>
2013-11-13 17:33:42 +0100

    New MainBubblesort

--- sortingalgorithm/src/sortingalgorithm/MainBubblesort.java
+++ sortingalgorithm/src/sortingalgorithm/MainBubblesort.java
@@ -5,8 +5,10 @@ public class MainBubblesort {
 	public static void main(String[] args) {
 
 		int numbers[] = { 6, 2, 8, 1, 4, 5, 7, 9, 3 };
+		int random [] = {10,20,9,50,66,81,47,36};
 
 		Bubblesort b = new Bubblesort(); // neue instanz erzeugt
+		Bubblesort c = new Bubblesort();
 
 		System.out.print("Unsortierte Werte\n");
 		printArray(numbers); // zuerst die Unsortierten Werte ausgeben
@@ -15,9 +17,14 @@ public class MainBubblesort {
 								// zeile Anfange
 
 		b.bubbleSort(numbers); // sortierten
-
+		c.bubbleSort(random);
+		
 		System.out.println("SortierteWerte");
 		printArray(numbers); // sortierte Werte ausgeben
+		
+		System.out.print("\n");
+		System.out.println("SortierteWerte");
+		printArray(random);
 
 	}
 
